7757376dabf797f8d19d9ce1490f9d15e7f354dd
SCHILD Erwan <erwan.schild@e-i.com>
2023-03-10 09:36:33 +0100

    ci: add manual ci checks

--- scripts/code-quality-checking.sh
+++ scripts/code-quality-checking.sh
@@ -3,23 +3,23 @@ set -e
 
 # Notebook to scripts
 echo ">> Apply notebook to scripts conversion"
-mkdir -p TEMP/
-jupyter nbconvert --to script --output-dir TEMP/ notebook/*.ipynb
+mkdir -p .temp/
+jupyter nbconvert --to script --output-dir .temp/ */notebook/*.ipynb
 
 # Format
 echo ">> Apply file formatting"
-isort notebook/
-black notebook/
+isort */notebook/
+black */notebook/
 
 # Type checking
 echo ">> Apply type checking"
-mypy --config-file config/mypy.ini notebook/
-mypy --config-file config/mypy.ini TEMP/
+mypy --config-file config/mypy.ini */notebook/
+mypy --config-file config/mypy.ini .temp/
 
 # Code quality checking
 echo ">> Apply code quality checking"
-flake8 --config=config/flake8.ini notebook/
-flake8 --config=config/flake8.ini TEMP/
+flake8 --config=config/flake8.ini */notebook/
+flake8 --config=config/flake8.ini .temp/
 
 # Done
 echo ">> DONE"
